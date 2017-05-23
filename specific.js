d8cc9096a698a74435570262e31b9b8559b8b729
rikumax25 <jorgev259@outlook.es>
2017-05-22 21:07:57 -0600

    fixed json syntax

--- specific.js
+++ specific.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+    $.ajax({
+        url: "./setup_steps",
+        async: false,
+        success: function (data){
+            start_setup(data);
+        }
+    });  
+})
+
+function start_setup(data){
+    console.log(JSON.parse(data));
     var list = $("#whatyouneed").children();
     console.log(list);
     console.log(list[0].dataset);
-})
+}
